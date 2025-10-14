32275871b286dffd94a0c813576fc4b0ddadfe88
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2025-10-15 00:15:34 +0800

    Updated SharedContext

--- include/Flex/Entity/SharedContext.hpp
+++ include/Flex/Entity/SharedContext.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include "Flex/Event/EventManager.hpp"
 
 namespace Flex {
 
 struct SharedContext {
-
+    EventManager* eventManager = nullptr;
 }; // struct SharedContext
 
 } // namespace Flex
