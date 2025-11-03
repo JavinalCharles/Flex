9e9b108e26fb5fc7e607ac25a6afc841641edd83
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2025-11-03 19:11:07 +0800

    Added System::setWorld()

--- include/Flex/Core/Systems/Systems.hpp
+++ include/Flex/Core/Systems/Systems.hpp
@@ -12,9 +12,9 @@ namespace Flex {
 			virtual void update(double dt) = 0;
 			virtual void postUpdate(double dt) = 0;
 
-			constexpr World* getWorld() const {
-				return m_world;
-			}
+			constexpr World* getWorld() const { return m_world; }
+			constexpr void setWorld(World* world) { m_world = world; }
+
 		protected:
 			[[maybe_unused]] World* m_world = nullptr;
 	}; // class System
